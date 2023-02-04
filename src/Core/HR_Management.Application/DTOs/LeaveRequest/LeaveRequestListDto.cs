2b1324281b72b1b5b84c4a7726cb5d6ced75f3c1
alijafari0572 <alijafari0572@gmail.com>
2023-02-04 14:34:37 +0330

    Create LeaveRequestListDto.cs

---
+++ src/Core/HR_Management.Application/DTOs/LeaveRequest/LeaveRequestListDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR_Management.Application.DTOs.LeaveRequest
+{
+    internal class LeaveRequestListDto
+    {
+    }
+}
