1beaab48923c514e5af778196e2b4f1c00838842
alijafari0572 <alijafari0572@gmail.com>
2023-06-04 10:47:24 +0330

    درس 62

---
+++ test/HR_Management.Test/LeaveTypes/Commands/CreateLeaveTypeCommandHandlerTests.cs
@@ -0,0 +1,54 @@
+﻿using AutoMapper;
+using HR_Management.Application.Contracts.Persistance;
+using HR_Management.Application.DTOs.LeaveType;
+using HR_Management.Application.Features.LeaveTypes.Handlers.Commands;
+using HR_Management.Application.Features.LeaveTypes.Requests.Commands;
+using HR_Management.Application.Profiles;
+using HR_Management.Application.UnitTests.Mocks;
+using Moq;
+using Shouldly;
+
+namespace HR_Management.Application.UnitTests.LeaveTypes.Commands
+{
+    public class CreateLeaveTypeCommandHandlerTests
+    {
+        IMapper _mapper;
+        Mock<ILeaveTypeRepository> _mockRepository;
+        CreateLeaveTypeDto _leaveTypeDto;
+        public CreateLeaveTypeCommandHandlerTests()
+        {
+            _mockRepository = MockLeaveRepository.GetLeaveTypeRepository();
+
+            var mapperConfig = new MapperConfiguration(m =>
+            {
+                m.AddProfile<MappingProfile>();
+            });
+
+            _mapper = mapperConfig.CreateMapper();
+
+            _leaveTypeDto = new CreateLeaveTypeDto()
+            {
+                DefaultDay = 15,
+                Name = "Test DTO"
+            };
+        }
+
+        [Fact]
+        public async Task CreateLeaveType()
+        {
+            var handler = new CreateLeaveTypeCommandtHandler(_mockRepository.Object, _mapper);
+            var result = await handler.Handle(new CreateLeaveTypeCommand()
+            {
+                CreateLeaveTypeDto = _leaveTypeDto
+            }, CancellationToken.None);
+
+            result.ShouldBeOfType<int>();
+
+            var leaveTypes = await _mockRepository.Object.GetAll();
+
+            leaveTypes.Count.ShouldBe(3);
+        }
+
+     
+    }
+}
